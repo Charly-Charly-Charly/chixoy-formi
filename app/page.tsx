a77001adebe05f466e997571295e3a4031aefebb
charly-charly-charly <calexanderlp7@gmail.com>
2025-11-10 11:12:45 -0600

    se agregó boton logout

--- app/page.tsx
+++ app/page.tsx
@@ -697,15 +697,15 @@ export default function Home() {
           <h1 className="text-2xl font-bold text-white">
             Reporte de Cumplimiento
           </h1>
-          {/* <div className="flex gap-2">
-            <button
+          <div className="flex gap-2">
+            {/* <button
               onClick={() => (window.location.href = "/admin")}
               className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition text-sm font-medium"
             >
               Panel Admin
-            </button>
+            </button> */}
             <LogoutButton />
-          </div> */}
+          </div>
         </div>
 
         {/* INSTITUTIONS VIEW */}
